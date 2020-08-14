72d84931eeeeb22741ad41253e3cfcad31a3c86c
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-08-14 09:51:33 +0300

    Добавил обработчик ServerSelectionTimeoutError

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ from bson.errors import InvalidId
 from bson import ObjectId
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
-
+from pymongo.errors import ServerSelectionTimeoutError
 
 app = Flask(__name__)
 
+
 def connect_db():
     client = MongoClient('mymongo_1', 27017)
-    db = client['test_bd']
+    db = client['sl_bd']
     return db['good']
+
+
+def work_with_db(method, obj):
+    try:
+        return method(obj)
+    except ServerSelectionTimeoutError:
+        return
 
 
 @app.route('/<id_good>', methods=['GET'])
 def get_good(id_good):
     goods_base = connect_db()
     try:
-        good = goods_base.find_one({'_id': ObjectId(id_good)})
-        return jsonify({'name': good['name']}), 200
+        good = work_with_db(goods_base.find_one, {'_id': ObjectId(id_good)})
+        if good:
+            return jsonify({'name': good['name']}), 200
+        return jsonify({'error': 'no database connection'}), 503
     except InvalidId:
         return jsonify({'error': 'good not found'}), 200
 
@@ -30,8 +40,11 @@ def create_good():
         return jsonify({'error': 'Invalid format data'}), 400
 
     good = {'name': request.json.get('name_good')}
-    result = goods_base.insert_one(good)
-    return jsonify({'_id': str(result.inserted_id), 'name': good['name']})
+    result = work_with_db(goods_base.insert_one, good)
+
+    if result:
+        return jsonify({'_id': str(result.inserted_id), 'name': good['name']}), 200
+    return jsonify({'error': 'no database connection'}), 503
 
 
 if __name__ == '__main__':
